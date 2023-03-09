2a299b95ae16fe0b88f552c56cbda039b1de28b2
caltam7 <101660165+caltam7@users.noreply.github.com>
2023-03-09 15:32:17 +0800

    Created new activity "newUserEnterInfo" for collecting new player's health info, i.e. B-day, gender, current weight. (Only finished temporary layout, the whole activity will soon be completed)

---
+++ app/src/main/java/com/example/petwalker/newUserEnterInfo.java
@@ -0,0 +1,14 @@
+package com.example.petwalker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class newUserEnterInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_user_enter_info);
+    }
+}

6225f6499d6ffee2d216edad9d6b933bc167415a
hugongck <hugocheukkei@gmail.com>
2023-03-28 16:16:14 +0800

    Combined step counter function in StepCount.java

--- app/src/main/java/com/example/petwalker/StepCount.java
+++ app/src/main/java/com/example/petwalker/StepCount.java
@@ -1,16 +1,29 @@
 package com.example.petwalker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
-import android.view.View;
-import android.widget.ProgressBar;
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
-import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class StepCount extends AppCompatActivity {
+public class StepCount extends AppCompatActivity implements SensorEventListener {
 
+    private TextView textViewStepCounter, textViewStepDetector;
+    private SensorManager sensorManager;
+    private Sensor mStepCounter;
+    private boolean isCounterSensorPresent;
+    int stepCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,9 +54,59 @@ public class StepCount extends AppCompatActivity {
             }
         });
 
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_DENIED){ //ask for permission
+            requestPermissions(new String[]{Manifest.permission.ACTIVITY_RECOGNITION}, 0);
+        }
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        textViewStepCounter = findViewById(R.id.txt_walked_step);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null)
+        {
+            mStepCounter = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+            isCounterSensorPresent = true;
+        }
+        else
+        {
+            textViewStepCounter.setText("-");
+            isCounterSensorPresent = false;
+        }
+
         //progress bar
         ProgressBar progressBar = findViewById(R.id.circular_progress_bar);
-        int progressValue = (int) ((float)1696/2000*100); // Set the progress value here
+        int progressValue = (int) ((float)stepCount/2000*100); // Set the progress value here
         progressBar.setProgress(progressValue);
     }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorevent) {
+        if(sensorevent.sensor == mStepCounter)
+        {
+            stepCount = (int) sensorevent.values[0];
+            textViewStepCounter.setText(String.valueOf(stepCount));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null)
+            sensorManager.registerListener(this, mStepCounter, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null)
+            sensorManager.unregisterListener(this, mStepCounter);
+    }
+
 }

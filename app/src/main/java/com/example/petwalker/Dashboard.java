45791e169ba8e1202b06a7571efa513ac3419e50
hugongck <hugocheukkei@gmail.com>
2023-04-03 14:47:21 +0800

    Updated greeting message in Dashboard and EPet

--- app/src/main/java/com/example/petwalker/Dashboard.java
+++ app/src/main/java/com/example/petwalker/Dashboard.java
@@ -4,15 +4,65 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 public class Dashboard extends AppCompatActivity {
+
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();;
+    private FirebaseManager fypDB = FirebaseManager.getInstance();
+    private DatabaseReference databaseRef = fypDB.getDatabaseRef();
+    private DatabaseReference currentUserRef;
+
+    private User currentUserData = new User();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        //Retrieve user data from database
+        String uid = mAuth.getCurrentUser().getUid();
+        currentUserRef = databaseRef.child("users").child(uid);
+        currentUserRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get the user data from the snapshot
+                User currentUserData = dataSnapshot.getValue(User.class);
+
+                // Use the retrieved values as needed
+                //Greeting message
+                TextView epetTalk = findViewById(R.id.epet_talk);
+                String currentDate = Time.getCurrentDate();
+                String currentTime = Time.getCurrentTime();
+                String currentDateTime = Time.getCurrentDateTime();
+                String currentTime_H = currentTime.substring(0, 2);
+                if(Integer.parseInt(currentTime_H) >= 0 && Integer.parseInt(currentTime_H) < 12){
+                    epetTalk.setText("Good Morning, "+currentUserData.getName());
+                }else if(Integer.parseInt(currentTime_H) >= 12 && Integer.parseInt(currentTime_H) < 16){
+                    epetTalk.setText("Good Afternoon, "+currentUserData.getName());
+                }else if(Integer.parseInt(currentTime_H) >= 16 && Integer.parseInt(currentTime_H) < 24){
+                    epetTalk.setText("Good Evening, "+currentUserData.getName());
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Handle errors
+                String TAG = "TAG: ";
+                Log.e(TAG, "Database error: " + databaseError.getMessage());
+                Toast.makeText(getApplicationContext(), "Database error: " + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
         // Hide the status bar.
         View decorView = getWindow().getDecorView();
@@ -79,6 +129,9 @@ public class Dashboard extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+
     }
 
     @Override
--- app/src/main/java/com/example/petwalker/epet.java
+++ app/src/main/java/com/example/petwalker/epet.java
@@ -10,6 +10,7 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ProgressBar;
@@ -17,10 +18,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.progressindicator.LinearProgressIndicator;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 public class epet extends AppCompatActivity implements SensorEventListener {
+
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();;
+    private FirebaseManager fypDB = FirebaseManager.getInstance();
+    private DatabaseReference databaseRef = fypDB.getDatabaseRef();
+    private DatabaseReference currentUserRef;
+
+    private User currentUserData = new User();
 
     private TextView textViewStepCounter, textViewStepDetector;
     private SensorManager sensorManager;
@@ -32,6 +46,40 @@ public class epet extends AppCompatActivity implements SensorEventListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_epet);
+
+        //Retrieve user data from database
+        String uid = mAuth.getCurrentUser().getUid();
+        currentUserRef = databaseRef.child("users").child(uid);
+        currentUserRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Get the user data from the snapshot
+                User currentUserData = dataSnapshot.getValue(User.class);
+
+                // Use the retrieved values as needed
+                //Greeting message
+                TextView epetTalk = findViewById(R.id.epet_talk);
+                String currentDate = Time.getCurrentDate();
+                String currentTime = Time.getCurrentTime();
+                String currentDateTime = Time.getCurrentDateTime();
+                String currentTime_H = currentTime.substring(0, 2);
+                if(Integer.parseInt(currentTime_H) >= 0 && Integer.parseInt(currentTime_H) < 12){
+                    epetTalk.setText("Good Morning, "+currentUserData.getName());
+                }else if(Integer.parseInt(currentTime_H) >= 12 && Integer.parseInt(currentTime_H) < 16){
+                    epetTalk.setText("Good Afternoon, "+currentUserData.getName());
+                }else if(Integer.parseInt(currentTime_H) >= 16 && Integer.parseInt(currentTime_H) < 24){
+                    epetTalk.setText("Good Evening, "+currentUserData.getName());
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Handle errors
+                String TAG = "TAG: ";
+                Log.e(TAG, "Database error: " + databaseError.getMessage());
+                Toast.makeText(getApplicationContext(), "Database error: " + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
         // Hide the status bar.
         View decorView = getWindow().getDecorView();
